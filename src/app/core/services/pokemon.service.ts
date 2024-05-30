a1eb1b2fbcca2696d27efb125bfdea793e750b14
Álefe Alves <alefealves@hotmail.com>
2024-05-29 23:26:08 -0400

    feat: adicionando funcoes para pegar o detalhe do pokemon e funcao para trazer toda a listagem de pokemons

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { Pokemon } from '../models/pokemon.list';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, forkJoin, map, mergeMap, throwError } from 'rxjs';
+import { PokemonDetail } from '../models/pokemon.detail';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,32 @@ export class PokemonService {
 
   getPokemonsList(offset: number, limit: number = 20): Observable<Pokemon[]> {
     return this.http.get<any>(`${this.baseUrl}?offset=${offset}&limit=${limit}`)
+      .pipe(
+        mergeMap(response => {
+          const requests = response.results.map((pokemon: any) =>
+            this.http.get<any>(pokemon.url).pipe(
+              map(details => ({
+                id: details.id,
+                name: details.name,
+                url: details.url,
+                isFavorite: false,
+                image: details.sprites.front_default,
+                gif: details.sprites.other.showdown.front_default,
+                types: details.types,
+              }))
+            )
+          );
+          return forkJoin(requests) as Observable<Pokemon[]>;
+        }),
+        catchError(error => throwError(error)));
+  }
+
+  getPokemonDetail(pokemon: number | string): Observable<PokemonDetail> {
+    return this.http.get<PokemonDetail>(`${this.baseUrl}/${pokemon}`);
+  }
+  
+  getAllPokemons(): Observable<Pokemon[]> {
+    return this.http.get<any>(`${this.baseUrl}?offset=0&limit=${this.totalPokemons}`)
       .pipe(
         mergeMap(response => {
           const requests = response.results.map((pokemon: any) =>
